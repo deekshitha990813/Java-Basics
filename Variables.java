ba817f3c60dd5c169c2f19952259e117ce427e1a
Deekshitha <bandadeekshitha22@gmail.com>
2026-06-29 21:24:54 +0530

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,13 @@
+public class Main
+  {
+    public static void main(String[] args)
+    {
+        String studentName = "Deekshitha";
+        int studentAge = 19;
+        int collegeYear = 2025;
+
+        System.out.println("Name: " + studentName);
+        System.out.println("Age: " + studentAge);
+        System.out.println("Year: " + collegeYear);
+    }
+}
