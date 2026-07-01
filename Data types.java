d0d20e3b6a5fc1f9139b188cd65e537b5b4889a9
Deekshitha <bandadeekshitha22@gmail.com>
2026-07-01 11:56:34 +0530

    Create Data types.java

---
+++ Data types.java
@@ -0,0 +1,14 @@
+public class DataTypes 
+{
+  public static void main(String[] args)
+  {
+    String myName = "Deekshitha";
+    int myAge = 18;
+    double mySGPA = 8.20;
+    boolean hosteller = false;
+    System.out.println("Name: " + myName);
+    System.out.println("Age: " + myAge);
+    System.out.println("SGPA: " + mySGPA);
+    System.out.println("Hosteller: " + hosteller);
+    }
+}
